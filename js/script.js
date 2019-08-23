bc81ea95181bba12f0c2ad632ade3716daa0b437
Marko Lalović <lalamarko1@gmail.com>
2019-08-23 14:48:43 +0200

    Fixed hover. Fixed footer.

--- js/script.js
+++ js/script.js
@@ -11,10 +11,4 @@ window.onresize = function(){
 	if (window.innerWidth > 1035) {
 		document.getElementById("burger").style.display = "none";
 	}
-}   
-		
-var bigNewsNumber = document.querySelectorAll('.news-big').length;
-if (bigNewsNumber % 2 == 0) {
-	var element = document.getElementById("double-small");
-   	element.classList.add("double-small");
 }
